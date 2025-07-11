e600cdc5b842043127e84baaf83180a8264a5fc8
Akinyemi Marvellous <akinyemioluwasegun2@gmail.com>
2025-07-10 22:46:04 -0700

    test site

---
+++ script.js
@@ -0,0 +1,41 @@
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+
+function openTab(tabName) {
+    for (tabLink of tabLinks) {
+        tabLink.classList.remove("active-link")
+    }
+    for (tabContent of tabContents) {
+        tabContent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabName).classList.add("active-tab")
+}
+
+
+var sideMenu = document.getElementById("sideMenu");
+
+function openMenu() {
+    sideMenu.style.right = "0"
+}
+function closeMenu() {
+    sideMenu.style.right = "-200px"
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwuUFDTNfAMYztZKTqzEj7C_Pe0HSH0h1WJzwCTXa98D8BETMcS_jhNh0g-N6vHemmj/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
